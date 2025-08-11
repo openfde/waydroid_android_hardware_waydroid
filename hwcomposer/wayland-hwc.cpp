399ec80c824819300dcb2ea7df328cd00498bbeb
huyang <1184394624@qq.com>
2025-08-11 19:17:02 +0800

    Revert "Fix: cursor cannot render after move out of screen when mouse event convert to touch event"
    This reverts commit d8f7abac9a8e992afcd6153c3b5260984c4850bc.

--- hwcomposer/wayland-hwc.cpp
+++ hwcomposer/wayland-hwc.cpp
@@ -1030,8 +1030,7 @@ pointer_handle_motion(void *data, struct wl_pointer *,
         ADD_EVENT(EV_SYN, SYN_REPORT, 0);
         display->ptrPrvX = x;
         display->ptrPrvY = y;
-        bool near_bord = x <= 10 || y <= 10 || x >= display->width - 10 || y >= display->height - 10;
-        if(property_get_bool("fde.inject_as_touch", false) && !near_bord){
+        if(property_get_bool("fde.inject_as_touch", false)){
             return;
         }
 
