db3db0c5dde7e695acf94e08345a27d556e11fe9
ironlvxyy <ironmanpeng@126.com>
2025-11-06 17:24:24 +0800

    define x11 keyboard key state

--- hwcomposerx11/x11-hwc.h
+++ hwcomposerx11/x11-hwc.h
@@ -99,6 +99,18 @@ enum {
     GRALLOC_FTG340
 };
 
+enum x11_keyboard_key_state {
+        /**
+         * key is not pressed
+         */
+        X11_KEYBOARD_KEY_STATE_RELEASED = 0,
+        /**
+         * key is pressed
+         */
+        X11_KEYBOARD_KEY_STATE_PRESSED = 1,
+};
+
+
 #define MAX_TOUCHPOINTS 10
 
 struct layerFrame {
